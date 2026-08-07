2d483ccf78a52b622f8490da359bf515653f81f7
MeierLink <1772791874@qq.com>
2026-08-07 21:02:44 +0800

    Update CuTe_1.cu
    补充注释和确定学习任务

--- CuTe_1.cu
+++ CuTe_1.cu
@@ -1,9 +1,14 @@
+/*学习CuTe的第一篇代码。 任务，学会定义一个layout。学会用不同的语法(make_tensor、make_layout)来定义Tensor
+学会如何访问Tensor中某个坐标
+学会从一个源Tensor上切出一个新Tensor并访问第3块*/
+
+
 #include <cuda_runtime.h>
 #include <iostream>
 #include <cute/tensor.hpp>
 
 using namespace std;
-
+//using namespace cute;
 
 
 
@@ -20,20 +25,20 @@ int main(){
 
     auto layout = make_layout(cute::make_shape(cute::Int<4>{},cute::Int<2>{}),cute::make_stride(cute::Int<2>{},cute::Int<1>{}));
 
-    auto Tensor_A = make_tensor(data,layout);
+    auto Tensor_A = make_tensor(data,layout);  //一个完整的Tensor = Pointer + Layout
 
-    cou<<Tensor(1,2)<<endl;  //打印第1行第2列的值，即3
+    cou<<Tensor_A(1,2)<<endl;  //打印第1行第2列的值，即3
 
     auto Row_1 = Tensor_A(1,_);  //切到第一行来 行里的步长仍为1
 
     auto tile_A = cute::make_layout(cute::make_shape(cute::Int<8>{},cute::Int<8>{}),cute::make_stride(cute::Int<8>{},cute::Int<1>{}));
 
-    auto tiler = cute::make_layout(cute::make_shape(cute::Int<4>{},cute::Int<4>{}),layoutRight{});  //自动以行主序填补Stride部分
+    auto tiler = cute::make_layout(cute::make_shape(cute::Int<4>{},cute::Int<4>{}),layoutRight{});  //自动以行主序填补Stride部分（layoutLeft会以列主序填补Stride）
 
-    auto tile_11 = cute::zipped_divide(tile_A,tiler);
+    auto tile_11 = cute::zipped_divide(tile_A,tiler);  //将源tileA矩阵以tiler形式划分  所以tile11的shape为((4,4),(2,2)) stride为((8,1),(32,1))
 
-    auto new_tile_11 = tile_11(cute::make_coord(_,_),cute::make_coord(1,1));
+    auto new_tile_11 = tile_11(cute::make_coord(_,_),cute::make_coord(1,1));  //因为tile11的右Tuple是嵌套形式所以要coord包裹形成对象
 
-    auto tiler_11 = cute::local_tile(tile_A,tiler,cute::make_coord(1,1));
+    auto tiler_11 = cute::local_tile(tile_A,tiler,cute::make_coord(1,1));  //localtile将34行到40行的代码一次性执行了，用它会更快捷
     
 }
