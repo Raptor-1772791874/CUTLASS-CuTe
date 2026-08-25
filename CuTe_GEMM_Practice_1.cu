5cad23b2989eeaf7b75163c7155f02f3ca269138
MeierLink <1772791874@qq.com>
2026-08-25 17:03:23 +0800

    Update CuTe_GEMM_Practice_1.cu
    修改错误，增加TiledCopy、推理Source Smem、Retile tcsA/B以Copy的view去观察

--- CuTe_GEMM_Practice_1.cu
+++ CuTe_GEMM_Practice_1.cu
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cuda_runtime.h>
 #include <cute/tensor.hpp>
-#include <cute/algorithm/gmem.hpp>
+#include <cute/algorithm/gemm.hpp>
 #include <cute/algorithm/copy.hpp>
 
 using namespace std;
@@ -17,7 +17,7 @@ __global__ void cute_gemm(half_t* const A,
 
         Tensor gA = make_tensor(make_gmem_ptr(A),Layout<Shape<_32,_16>,Stride<_16,_1>>{});
         Tensor gB = make_tensor(make_gmem_ptr(B),Layout<Shape<_32,_16>,Stride<_16,_1>>{});
-        Tensor gC = make_tnesor(make_gmem_ptr(C),Layout<Shape<_32,_32>,Stride<_32,_1>>{});
+        Tensor gC = make_tensor(make_gmem_ptr(C),Layout<Shape<_32,_32>,Stride<_32,_1>>{});
 
 
         __shared__ half_t sA[32*16];
@@ -30,6 +30,7 @@ __global__ void cute_gemm(half_t* const A,
         }
 
 
+
         TiledMMA tiledmma = make_tiled_mma(SM80_16x8x16_F32F16F16F32_TN{},
             Layout<Shape<_2,_2>>{},
             Tile<_32,_32,_16>{});
@@ -39,13 +40,34 @@ __global__ void cute_gemm(half_t* const A,
 
 
             Tensor tcrA = thr_mma.partition_fragment_A(sA);
-            Tnesor tcrB = thr_mma.partition_fragment_B(sB);
+            Tensor tcrB = thr_mma.partition_fragment_B(sB);
 
             Tensor tcgC = thr_mma.partition_C(gC);
+            Tensor tcrC = thr_mma.partition_fragment_C(tcgc);
 
 
-            Copy_Atom<SM75_U32x4_LDSM_N{}> copyAtom_A;
-            Copy_Atom<SM75_U32x4_LDSM_N{}> copyAtom_B;
+
+
+            Copy_Atom<SM75_U32x4_LDSM_N,half_t> copyAtom_A;
+            Copy_Atom<SM75_U32x4_LDSM_N,half_t> copyAtom_B;
+
+
+
+            TiledCopy tiledcopyA = make_tiled_copy_A(copyAtom_A,tiledmma);
+            TiledCopy tiledcopyB = make_tiled_copy_B(coptAtom_B,tiledmma);
+
+
+            ThrCopy thrcopyA = tiledcopyA.get_slice(threadIdx.x);
+            ThrCopy thrcopyB = tiledcopyB.get_slice(threadIdx.x);
+
+
+            Tensor txsA = thrcopyA.partition_S(sA);
+            Tensor txsB = thrcopyB.partition_S(sB);
+
+
+            Tensor txrA = thrcopyA.retile_D(tcsA);
+            Tensor txrB = thrcopyB.retile_D(tcsB);
+
 
 
 
