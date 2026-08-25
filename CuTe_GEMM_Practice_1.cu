256d2c8927cc6a812b61fe702eb872b8afa0de8b
MeierLink <1772791874@qq.com>
2026-08-25 15:58:47 +0800

    Create CuTe_GEMM_Practice_1.cu
    CuTe主要语法学习后，实践从baseLine开始尝试写GEMM。第一版不追求各种技法，只完成基础定义、数据搬运、TiledMMA/Copy、Retile、Gemm。

---
+++ CuTe_GEMM_Practice_1.cu
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cuda_runtime.h>
+#include <cute/tensor.hpp>
+#include <cute/algorithm/gmem.hpp>
+#include <cute/algorithm/copy.hpp>
+
+using namespace std;
+using namespace cute;
+
+
+ //  A/B [32][16] C[32][32]
+
+
+__global__ void cute_gemm(half_t* const A,
+    half_t* const B,
+    float* C){
+
+        Tensor gA = make_tensor(make_gmem_ptr(A),Layout<Shape<_32,_16>,Stride<_16,_1>>{});
+        Tensor gB = make_tensor(make_gmem_ptr(B),Layout<Shape<_32,_16>,Stride<_16,_1>>{});
+        Tensor gC = make_tnesor(make_gmem_ptr(C),Layout<Shape<_32,_32>,Stride<_32,_1>>{});
+
+
+        __shared__ half_t sA[32*16];
+        __shared__ half_t sB[32*16];
+
+
+        for(int i=threadIdx.x;i<32*16;++blockDim.x){
+            sA[i] = gA[i];
+            sB[i] = gB[i];
+        }
+
+
+        TiledMMA tiledmma = make_tiled_mma(SM80_16x8x16_F32F16F16F32_TN{},
+            Layout<Shape<_2,_2>>{},
+            Tile<_32,_32,_16>{});
+
+
+            ThrMMA thr_mma = tiledmma.get_slice(threadIdx.x);
+
+
+            Tensor tcrA = thr_mma.partition_fragment_A(sA);
+            Tnesor tcrB = thr_mma.partition_fragment_B(sB);
+
+            Tensor tcgC = thr_mma.partition_C(gC);
+
+
+            Copy_Atom<SM75_U32x4_LDSM_N{}> copyAtom_A;
+            Copy_Atom<SM75_U32x4_LDSM_N{}> copyAtom_B;
+
+
+
+
+
+}
+
+
+
+int main(){
+
+
+
+
+    return 0;
+}
